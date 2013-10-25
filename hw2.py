ff045a52313f5ad939efa8c5a78ef065f0b556da
aarmag22 <mlaaron@gmail.com>
2013-10-25 14:41:32 -0700

    Create hw2.py

---
+++ hw2.py
@@ -0,0 +1,103 @@
+# Name: Maggie Aaron
+# Evergreen Login: aarmag22
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 2
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+
+###
+### Problem 1      
+### Gauss's Problem: Adds natural numbers from 'i' thru 'n' and prints sum.
+
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 1 solution follows:"
+
+sum = 0            # running total of values
+i = 1              # initial value
+n = 100            # final value
+
+while i < n + 1:   # must use n + 1 to include the final value
+    sum = sum + i  
+    i = i + 1
+    
+print sum
+
+
+###
+### Problem 2
+### Reciprocals: Prints the decimal values of 1 / n
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 2 solution follows:"
+
+x = 2             # initial value
+y = 10            # final value
+
+for i in range (x,y+1):
+    r = 1.0 / x
+    print r
+    x = x + 1
+
+###
+### Problem 3
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 3 solution follows:"
+
+n = 10              # final value
+triangular = 0      # initial value
+for i in range(0,n):
+    triangular = 
+print "Triangular number", n, "via loop:", triangular
+print "Triangular number", n, "via formula:", (n*(n+1))/2
+
+###
+### Problem 4
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 4 solution follows:"
+
+n = 10
+for n in range(n,n+1):
+    print math.factorial(n)
+
+###
+### Problem 5
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 5 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 6
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 6 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Collaboration
+###
+
+# ... List your collaborators and other sources of help here (websites, books, etc.),
+# ... as a comment (on a line starting with "#").
+
+###
+### Reflection
+###
+
+# ... Write how long this assignment took you, including doing all the readings
+# ... and tutorials linked to from the homework page. Did the readings, tutorials,
+# ... and lecture contain everything you needed to complete this assignment?
