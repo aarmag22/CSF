44cb3148780f0242c97ea9e0d3f03184f4381fde
aarmag22 <mlaaron@gmail.com>
2013-10-29 14:04:43 -0700

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -84,7 +84,7 @@ numlines = 10
 for j in range (numlines + 1):
     n = (numlines) - j
     product = 1
-    for i in range (1, n+1):
+    for i in range (1, n + 1):
         product = product * i
     print product
 
