e512d6c0f53a2dc9ddda56bc7689527abf3fe58b
Maggie <mlaaron@gmail.com>
2013-10-29 21:31:46 -0700

    Corrected and updated

--- hw1.py
+++ hw1.py
@@ -1,5 +1,5 @@
-# Name: Maggie Aaron & Brad Mattix
-# Evergreen Login: aarmag22, matbra30
+# Name: Maggie Aaron
+# Evergreen Login: aarmag22
 # Computer Science Foundations
 # Programming as a Way of Life
 # Homework 1
@@ -9,7 +9,7 @@
 # you are done.  When you are done, running this file should compute and
 # print the answers to all the problems.
 
-import math                     # makes the math.sqrt function available
+import math         # makes the math.sqrt function available
 
 
 ###
@@ -18,8 +18,12 @@ import math                     # makes the math.sqrt function available
 
 print "Problem 1 solution follows:"
 
-y = 5.86 + math.sqrt((5.86 ** 2) - (4 * (1) * (8.5408)))/(2 * (1))
-z = 5.86 - math.sqrt((5.86 ** 2) - (4 * (1) * (8.5408)))/(2 * (1))
+a = 1
+b = -5.86
+c = 8.5408
+
+y = -b + math.sqrt((b ** 2) - (4 * (a) * (c)))/(2 * (a))
+z = -b - math.sqrt((b ** 2) - (4 * (a) * (c)))/(2 * (a))
 
 print y
 print z
@@ -31,6 +35,7 @@ print z
 print "Problem 2 solution follows:"
 
 import hw1_test
+
 print hw1_test.a
 a = hw1_test.a    #sets a as variable from hw1_test
 print hw1_test.b
