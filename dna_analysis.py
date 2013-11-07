9955b7c548c57e2ead153e2d7934b60de68a5338
aarmag22 <MLA@Maggies-MacBook-Pro.local>
2013-11-06 23:14:06 -0800

    hw4
    Homework 4 completed

--- dna_analysis.py
+++ dna_analysis.py
@@ -3,6 +3,7 @@
 # Computer Science Foundations
 # Programming as a Way of Life
 # Homework 3: DNA analysis (Part 1)
+# Homework 4: DNA analysis (Part 2)
 
 # This program reads DNA sequencer output and computes statistics, such as
 # the GC content.  Run it from the command line like this:
@@ -49,13 +50,25 @@ for line in inputfile:
 ### Compute statistics
 ###
 
+
 # Total nucleotides seen so far.
 total_count = 0
+
 # Number of G and C nucleotides seen so far.
 gc_count = 0
+# Number of A and T nucleotides seen so far.
+at_count = 0
 
+# Number of A nucleotides seen so far.
+a_count = 0
+# Number of C nucleotides seen so far.
+c_count = 0
+# Number of G nucleotides seen so far.
+g_count = 0
+# Number of T nucleotides seen so far.
+t_count = 0
 
-# for each base pair in the string,
+# For each base pair in the string,
 for bp in seq:
     # increment the total number of bps we've seen
     total_count = total_count + 1
@@ -64,36 +77,59 @@ for bp in seq:
     if bp == 'C' or bp == 'G':
         # increment the count of gc
         gc_count = gc_count + 1
-
-
-# divide the gc_count by the total_count
-gc_content = float(gc_count) / total_count
-
-#########################################################################
-
-# Total nucleotides seen so far.
-total_count2 = 0
-# Number of A and T nucleotides seen so far.
-at_count = 0
-
-
-# for each base pair in the string,
-for bp in seq:
-    # increment the total number of bps we've seen
-    total_count2 = total_count2 + 1
-
-    # next, if the bp is a A or a T,
-    if bp == 'A' or bp == 'T':
+    # if the bp is an A or a T,
+    elif bp == 'A' or bp == 'T':
         # increment the count of at
         at_count = at_count + 1
+        
+    # if the nucleotide is an A
+    if bp == 'A':
+        # increment the count of a
+        a_count = a_count + 1
+    # if the nucleotide is a C
+    if bp == 'C':
+        # increment the count of c
+        c_count = c_count + 1
+    # if the nucleotide is a G
+    if bp == 'G':
+        # increment the count of g
+        g_count = g_count + 1
+    # if the nucleotide is a T
+    if bp == 'T':
+        # increment the count of t
+        t_count = t_count + 1
+        
+# Total count of A, C, G, and T nucleotides
+sum_count = a_count + c_count + g_count + t_count        
+# Sequence length 
+seq_length = len(seq)
 
 
-# divide the gc_count by the total_count
-at_content = float(at_count) / total_count2
+# Divide the gc_count by the sum_count
+gc_content = float(gc_count) / sum_count
+# Divide the at_count by the sum_count
+at_content = float(at_count) / sum_count
 
+# Compute the AT/GC ratio
+atgc_ratio = float(at_count)/(gc_count)
 
-# Print the answer
+# Categorize organisms
+if (gc_content > .60):
+    gc_class = 'high GC content'
+elif (gc_content < .40):
+    gc_class = 'low GC content'
+else:
+    gc_class = 'moderate GC content'
+
+# Print the answers
 print 'GC-content:', gc_content
-
-# Print the answer
 print 'AT-content:', at_content
+print 'A-count:', a_count
+print 'C-count:', c_count
+print 'G-count:', g_count
+print 'T-count:', t_count
+print 'Sum count:', sum_count
+print 'Total count:', total_count
+print 'seq length:', seq_length
+print 'AT/GC Ratio:', atgc_ratio
+print 'GC Classification:', gc_class
